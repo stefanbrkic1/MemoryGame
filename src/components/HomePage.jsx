819b15cff4ae4332c10b7be55d5ba4a6014879e9
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-12-29 13:35:32 +0100

    Added Home Buttons

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 function HomePage({ setActivePage }) {
+  const [soundState, setSoundState] = useState('off');
+
+  function handleSound() {
+    soundState === 'off' ? setSoundState('on') : setSoundState('off');
+  }
+
+  function handleInfo() {}
   return (
     <div className="home-container">
       {/* Header section */}
@@ -21,6 +30,31 @@ function HomePage({ setActivePage }) {
         </a>
         <div className="github-logo"></div>
       </footer>
+
+      {/*Sound and Info Buttons */}
+      <button
+        type="button"
+        className="home-buttons sound-btn"
+        onClick={handleSound}
+      >
+        <div
+          className="sound-btn-icon"
+          style={{
+            backgroundImage: `url(${
+              soundState === 'off'
+                ? '../src/assets/img/sound-on.svg'
+                : '../src/assets/img/sound-off.svg'
+            })`,
+          }}
+        ></div>
+      </button>
+      <button
+        type="button"
+        className="home-buttons info-btn"
+        onClick={handleInfo}
+      >
+        <div className="info-btn-icon"></div>
+      </button>
     </div>
   );
 }
