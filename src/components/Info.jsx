3d876cf9d15c9a25000b907a358bbec0469aac4a
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-01-07 17:59:30 +0100

    Updated Info For Levels

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -37,10 +37,17 @@ function InfoModal({ playClickSound }) {
               your memory skills in this thrilling card-memory game. Here's how
               to play:
             </div>
-            <div className="how-to">1. You get a deck of shuffled cards.</div>
-            <div className="how-to">2. Don't click on the same card twice!</div>
-            <div className="how-to">
-              3. Increase the score by going as many rounds as you can
+            <div className="how-to-container">
+              <div className="how-to">1. You get a deck of shuffled cards.</div>
+              <div className="how-to">
+                2. Don't click on the same card twice to complete level!
+              </div>
+              <div className="how-to">
+                3. Difficulty increases with each new level (6 new cards)
+              </div>
+            </div>
+            <div className="info-para">
+              There are 🏆6 levels of difficulty, see how far you can get!
             </div>
           </div>
         </div>
